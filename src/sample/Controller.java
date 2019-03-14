8c2fa4a3dd4c80367b7e0e870c5c9e0ded4be7b7
Sullivan <soconnor5921@bths.edu>
2019-03-14 16:38:18 -0400

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.beans.EventHandler;
+
+public class Controller
+{
+    @FXML
+    private Button scanWords;
+
+    public static void callRecognizer()
+    {
+        
+    }
+}
